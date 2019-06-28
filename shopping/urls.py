ab3148d024def3b7bf947a10b220ffaf34d1fa8f
a-foster <adam.foster93@hotmail.co.uk>
2019-06-28 08:22:20 +0100

    Added a change_category view

--- shopping/urls.py
+++ shopping/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'shopping'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:product_id>/', views.product_details, name='product_details'),
+    path('<int:product_id>/change_category/', views.change_category, name='change_category'),
 ]
--- shopping/views.py
+++ shopping/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Product
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from .models import Product, ProductCategory
 
 def index(request):
     all_added_products = Product.objects.filter(product_added=True)
     context = {'all_added_products': all_added_products}
     return render(request, 'shopping/index.html', context)
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'shopping/detail.html', {'product': product})
+
+def change_category(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    try:
+        new_category = ProductCategory.objects.filter(pk=request.POST['category'])
+    except (KeyError, Product.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'shopping/detail.html', {
+            'product': product,
+            'error_message': "You didn't select a category.",
+        })
+    else:
+        product.product_category_code = new_category
+        product.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('shopping:product_details', args=(product.id)))
+
+def product_details(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'shopping/product_details.html', {'product': product})
+    # return HttpResponse(str('Shite'))
